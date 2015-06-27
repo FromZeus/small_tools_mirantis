fd037de64a7ae8b76463d846309d0073984ff938
dtrishkin <dtrishkin@mirantis.com>
2015-06-27 16:27:05 +0300

    In progress...

--- dependency_list_former/form.py
+++ dependency_list_former/form.py
@@ -2,15 +2,18 @@ import argparse
 import re
 import json
 from os import listdir
-from os.path import join, isdir
+from os.path import join, isdir, abspath
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', dest='config', help='Configuration YAML')
 args = parser.parse_args()
 
-re_import = re.compile("(import)+\s+\S")
-re_from = re.compile("(from)+\s+\S")
-re_tail = re.compile("\S$")
+re_all_imports = re.compile(
+  "(from\s+\S+\s+)*((\\\\)+\s*\n\s*)*(import)+\s+\S+",
+    re.VERBOSE | re.MULTILINE)
+re_import = re.compile("(import)+\s+\S+")
+re_from = re.compile("(from)+\s+\S+")
+re_tail = re.compile("\S+$")
 
 def main():
   try:
@@ -26,28 +29,31 @@ def main():
     print '\nThe process was interrupted by the user'
     raise SystemExit
 
-def get_pack(line):
-  from_line = re_from.search(line)
-  import_line = re_import.search(line)
-  if from_line:
-
+def get_all_imports(text):
+  all_matches = []
+  for match in re_all_imports.finditer(text):
+    all_matches.append(match.group(0))
+  return all_matches
 
 def search_py(name, path):
   for _file in listdir(path):
-    if is 
+    pass
 
 def parse_py(path):
   with open(path) as py_file:
+    text = py_file.read()
+    get_all_imports(text)
     for line in py_file:
-      if "import" in line:
-        if 
+      pass
 
 def build_init_dir_tree(path):
   tree = dict()
   for _file in listdir(path):
     new_path = join(path, _file)
-    if isdir(_file) and "__init__.py" in listdir(new_path):
-      tree[new_path].add(build_init_dir_tree(new_path))
+    if isdir(new_path) and "__init__.py" in listdir(new_path):
+      if not new_path in tree:
+        tree[new_path] = []
+      tree[new_path].append(build_init_dir_tree(new_path))
   return tree
 
 if __name__ == '__main__':
--- dependency_list_former/test_mod.py
+++ dependency_list_former/test_mod.py
@@ -3,6 +3,12 @@ import re
 import json
 from os import listdir
 from os.path import join, isdir
+from test1.test2.test3 \
+  import test_lol
+
+re_all_imports = re.compile(
+  "(from\s+\S+\s+)*((\\\\)+\s*\n\s*)*(import)+\s+\S+",
+    re.VERBOSE | re.MULTILINE)
 
 def build_init_dir_tree(path):
   tree = dict()
