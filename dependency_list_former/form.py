b91d59a6eac586d2e1148b407aa3050837c746fb
dtrishkin <dtrishkin@mirantis.com>
2015-06-29 19:08:08 +0300

    Close to finish...

--- dependency_list_former/form.py
+++ dependency_list_former/form.py
@@ -1,8 +1,10 @@
 import argparse
 import re
 import json
+import yaml
 from os import listdir
 from os.path import join, isdir, abspath
+import pdb
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', dest='config', help='Configuration YAML')
@@ -15,20 +17,24 @@ re_import = re.compile("(import)+\s+\S+")
 re_from = re.compile("(from)+\s+\S+")
 re_tail = re.compile("\S+$")
 re_to_dot = re.compile("^[^\s.]+")
-re_to_backslash_from_end = re.compile("[^\s(\\\\)]$")
+re_to_last_slash = re.compile("\/*([^\/]+\/)+")
+re_to_slash_from_end = re.compile("[^\s\/]+$")
+#re_to_backslash_from_end = re.compile("[^\s(\\\\)]$")
 
 def main():
   try:
     conf = open(args.config, 'r')
     tempConf = yaml.load_all(conf)
 
+    pdb.set_trace()
+
     for line in tempConf:
       py_file_path = line["PyFilePath"]
 
     init_dir_tree = build_init_dir_tree(
-      "{0}/..".format(py_file_path))
+      re_to_last_slash.search(py_file_path).group(0))
 
-
+    print parse_py(py_file_path, init_dir_tree)
 
   except KeyboardInterrupt:
     print '\nThe process was interrupted by the user'
@@ -40,32 +46,44 @@ def get_all_imports(text):
     all_matches.append(match.group(0))
   return all_matches
 
-def extract_module(module_name, init_dir_tree):
-  module_name_start = re_to_dot.search(module_name).group(0)
-  module_name_end = module_name[len(module_name_start):]
+def extract_module(from_module, imp_module, init_dir_tree):
+  from_module_start = re_to_dot.search(from_module).group(0)
+  from_module_end = from_module[len(module_name_start):]
   dir_modules = []
-  for key in init_dir_tree.keys():
-    dir_modules.append(
-      re_to_backslash_from_end.search(key).group(0))
-  if init_dir_tree in dir_modules:
-    return extract_module(module_name_end, )
+  for key, tree in init_dir_tree.iteritems():
+    if from_module_start in key:
+      return extract_module(from_module_end, tree)
+    else:
+      return from_module_start
+
+def in_pyes(module, path):
+  files = listdir(path)
+  py_files = [el for el in files if el.endswith(".py")]
+  if module in ",".join(py_files):
+    return True
   else:
-    return module_name_start
+    return False
 
 def parse_py(path, init_dir_tree):
   modules = set()
+  cur_dir = re_to_last_slash.search(path).group(0)
   with open(path) as py_file:
     text = py_file.read()
     all_imports = get_all_imports(text)
     for imp in all_imports:
-      from_module = re_from(imp)
+      from_module = re_from.search(imp)
+      imp_module = re_to_dot.search(
+        re_tail.search(imp).group(0)).group(0)
       if not from_module:
-        imp_module = re_to_dot.search(
-          re_tail.search(imp).group(0)).group(0)
-        modules.add(imp_module)
+        if not in_pyes(imp_module, cur_dir):
+          modules.add(imp_module)
+        else:
+          modules |= parse_py(
+            "{0}/{1}.py".format(cur_dir, imp_module), init_dir_tree)
       else:
         from_module = re_tail.search(from_module.group(0)).group(0)
-        modules.add(extract_module(from_module, init_dir_tree))
+        modules.add(extract_module(
+          from_module, imp_module, init_dir_tree))
   return modules
 
 def build_init_dir_tree(path):
