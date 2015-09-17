4f0aa350339920d48641c9eab46e152fd1bee42a
dtrishkin <dtrishkin@mirantis.com>
2015-09-17 21:44:20 +0300

    Add "Fast Epoch Checker"

---
+++ fast_epoch_checker/Readme.rst
@@ -0,0 +1,19 @@
+=====================
+Fast Epoch Checker
+=====================
+
+Description
+-----------
+
+This utility is designed for fast checking epoch in rpm|deb - specs|control files. (At this moment only rpm is avaliable)
+
+How to use
+----------
+
+You can specify `config.yaml`_ if you want check authorized and do "python echeck.py -c config.yaml" or just run "python echeck.py" without any configuration.
+
+config.yaml
+^^^^^^^^^^^
+
+* Username - Your github login
+* Password - Your github e-mail
---
+++ fast_epoch_checker/echeck.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import re
+from StringIO import StringIO
+import base64
+import yaml
+import argparse
+
+re_spec = re.compile("[a-zA-Z0-9-_]+.spec")
+re_epoch = re.compile("\d+")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', dest='config', help='Configuration YAML')
+args = parser.parse_args()
+
+
+def get_spec_name(url):
+  page = requests.get(url, stream = True)
+  for line in page.text.split('\n'):
+    spec = re_spec.search(line)
+    if spec:
+      return spec.group(0)
+  return None
+
+
+def main():
+  try:
+    username = password = ""
+    
+    if args.config:
+      conf = open(args.config, 'r')
+      tempConf = yaml.load_all(conf)
+
+      for line in tempConf:
+        username = line["Username"]
+        password = line["Password"]
+
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    if username and password:
+      headers["Authorization"] = "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password))
+
+    repos_trash = \
+      requests.get("https://api.github.com/orgs/openstack-packages/repos?per_page=9999",
+        stream = True, headers = headers)
+    repos_info_list = json.loads(repos_trash.text)
+  
+    for el in repos_info_list:
+      buf_repos_info = json.dumps(el)
+      repos_info = json.loads(buf_repos_info)
+      if repos_info["default_branch"] == "rpm-master":
+        spec_name = get_spec_name("https://github.com/openstack-packages/{0}".format(repos_info["name"]))
+        for line in requests.get(
+          "https://raw.githubusercontent.com/openstack-packages" \
+          "/{0}/rpm-master/{1}".format(repos_info["name"], spec_name), stream = True).text.split('\n'):
+          if "Epoch:" in line:
+            epoch = re_epoch.search(line)
+            if epoch:
+              print "{0} : {1}".format(repos_info["name"], epoch.group(0))
+  except KeyboardInterrupt:
+    print '\nThe process was interrupted by the user'
+    raise SystemExit
+
+
+if __name__ == '__main__':
+  main()
